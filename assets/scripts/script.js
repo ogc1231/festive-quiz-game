b5496894c1a3c0d04d4c3639ec92d3bd11adb47a
Daniel Callaghan <dan_cal89@hotmail.com>
2023-12-15 01:31:01 +0000

    feat(trivia api): add example of api call to get trivia questions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,16 @@
 /* jshint esversion: 11 */
+
+const queryParams = {
+    type: 'multiple',
+    difficulty: 'easy',
+    amount: 12
+  };
+  
+  const queryString = Object.keys(queryParams)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
+    .join('&');
+  
+  fetch(`https://trivia-api-jsmx.onrender.com/trivia?${queryString}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
